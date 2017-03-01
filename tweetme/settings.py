4e918b7abb0f3d43dbb4323f9de236a271d4654f
Diego Ubirajara <diego.ubirajara@mail.com>
2017-03-01 19:24:30 +0100

    add posgresql

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tweetme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tweetmedb',
+        'USER': 'tweetmeadmin',
+        'PASSWORD': 'tweetmepass',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
