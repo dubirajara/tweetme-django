4943df2b768c39e687211687ac91202f45ac4b2b
Diego Ubirajara <diego.ubirajara@mail.com>
2017-03-21 19:46:30 +0100

    add test modelform

--- tweetme/tweets/tests.py
+++ tweetme/tweets/tests.py
@@ -3,6 +3,7 @@ from django.shortcuts import resolve_url as r
 
 from model_mommy import mommy
 
+from .forms import TweetModelForm
 from .models import Tweet
 
 
@@ -58,3 +59,25 @@ class CreateViewTest(TestCase):
     def test_html(self):
         """'create View' must use template create_view.html'"""
         self.assertTemplateUsed(self.response, 'tweets/create_view.html')
+
+
+class TweetsFormTest(TestCase):
+    def setUp(self):
+        self.form = TweetModelForm()
+
+    def test_form_has_fields(self):
+        """Tweets_Form must have 1 fields"""
+        expected = ('content',)
+        self.assertSequenceEqual(expected, list(self.form.fields))
+
+    def test_all_required_form_fields(self):
+        """Test Tweets_form field is required."""
+        form = TweetModelForm({
+            'content': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors)
+
+    def test_fields_not_present(self):
+        """Tweets_form field is not present."""
+        self.assertFalse(self.form.fields.get('user'))
