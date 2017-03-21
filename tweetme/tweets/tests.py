20077944e4b2ec032e97ca03ce0bb68d255a2c03
Diego Ubirajara <diego.ubirajara@mail.com>
2017-03-21 19:37:39 +0100

    add test create view

--- tweetme/tweets/tests.py
+++ tweetme/tweets/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+
 from model_mommy import mommy
+
 from .models import Tweet
 
 
 class ListViewTest(TestCase):
     def setUp(self):
         self.response = self.client.get(r('list'))
-
 
     def test_get(self):
         """GET 'list View' must return status code 200"""
@@ -22,7 +23,6 @@ class DetailViewTest(TestCase):
     def setUp(self):
         self.tweet = mommy.make('Tweet', pk=2)
         self.response = self.client.get(self.tweet.get_absolute_url())
-
 
     def test_get(self):
         """GET 'detail View' must return status code 200"""
@@ -45,3 +45,16 @@ class ModelTest(TestCase):
 
     def test_str(self):
         self.assertEqual(self.tweet.content, str(self.tweet))
+
+
+class CreateViewTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('create'))
+
+    def test_get(self):
+        """GET 'create View' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_html(self):
+        """'create View' must use template create_view.html'"""
+        self.assertTemplateUsed(self.response, 'tweets/create_view.html')
