6d783b524c4b65fb5cbb6013258295cea69f603b
Diego Ubirajara <diego.ubirajara@mail.com>
2017-04-03 19:49:36 +0200

    add updateview tests

--- tweetme/tweets/tests/tests_views.py
+++ tweetme/tweets/tests/tests_views.py
@@ -43,5 +43,30 @@ class CreateViewTest(TestCase):
         self.assertEqual(200, self.response.status_code)
 
     def test_html(self):
-        """'create View' must use template create_view.html'"""
+        """'create View' must use template create_view.html and form.html'"""
         self.assertTemplateUsed(self.response, 'tweets/create_view.html')
+        self.assertTemplateUsed(self.response, 'tweets/form.html')
+
+    def test_csrf(self):
+        """HTML must contain csrf"""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+
+class UpdateViewTest(TestCase):
+    def setUp(self):
+        self.tweet = mommy.make('Tweet', pk=2)
+        self.response = self.client.get(r('update', self.tweet.id))
+
+    def test_get(self):
+        """GET 'update View' must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_html(self):
+        """'update View' must use template update_view.html and form.html"""
+        self.assertTemplateUsed(self.response, 'tweets/update_view.html')
+        self.assertTemplateUsed(self.response, 'tweets/form.html')
+
+
+    def test_csrf(self):
+        """HTML must contain csrf"""
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
