57f4b632d9c737ce4b3db5e95b99b816fe252cbe
Diego Ubirajara <diego.ubirajara@mail.com>
2017-06-18 11:29:19 +0200

    update travis

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -79,8 +79,6 @@ WSGI_APPLICATION = 'tweetme.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tweetme',
-        'USER': 'tweetmeadmin',
         'HOST': '127.0.0.1',
     }
 }
