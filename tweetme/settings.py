fe7f89415d04a8d3bfa4515d0488f57bd9f06b1d
Diego Ubirajara <diego.ubirajara@mail.com>
2017-06-19 19:26:23 +0200

    update travis.

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -78,12 +78,19 @@ WSGI_APPLICATION = 'tweetme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'tweetme',
-        'USER': 'tweetmeadmin',
-        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'tweetme',
+#         'USER': 'tweetmeadmin',
+#         'HOST': '127.0.0.1',
+#     }
+# }
 
 
 # Password validation
