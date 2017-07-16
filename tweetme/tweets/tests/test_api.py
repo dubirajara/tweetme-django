929ce4708de3b03a3acfdf9dcac102809c139b54
Diego Ubirajara <diego.ubirajara@mail.com>
2017-07-16 11:01:46 +0200

    update test api serializer json

--- tweetme/tweets/tests/test_api.py
+++ tweetme/tweets/tests/test_api.py
@@ -15,11 +15,13 @@ class TweetListApiViewTest(APITestCase):
         self.user = 'diego'
         self.email = 'test@djangoapp.com'
         self.password = 'test'
+
         user = User.objects.create_user(
-            self.user, self.email, self.password
+            self.user, self.email, self.password,
+            first_name='Diego', last_name='Test',
         )
         self.tweet = Tweet.objects.create(
-            user=user, content='test app'
+            user=user, content='test app',
         )
 
         self.response = self.client.get(r('tweet-api:list'))
@@ -28,3 +30,12 @@ class TweetListApiViewTest(APITestCase):
     def test_api_get(self):
         """GET 'list api' must return status code 200"""
         self.assertEqual(status.HTTP_200_OK, self.response.status_code)
+
+    def test_api_contents(self):
+        """Test request must contain and return json contents"""
+        self.assertEqual(self.get_json, [{'content': 'test app',
+                                          'user': {'email': 'test@djangoapp.com',
+                                                   'first_name': 'Diego',
+                                                   'follower_count': 0,
+                                                   'last_name': 'Test',
+                                                   'username': 'diego'}}])
